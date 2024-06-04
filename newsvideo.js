04457321c4153eaaa2854d98c79fb97211732d2a
2N2T <158759960+2N2T@users.noreply.github.com>
2024-06-04 13:41:54 +0300

    Update 1.0012
    Finished the main idea, planning tons of new features in the future

---
+++ newsvideo.js
@@ -0,0 +1,27 @@
+  // Replace 'YOUR_API_KEY' and 'PLAYLIST_ID' with your actual API key and playlist ID
+  const apiKey = 'AIzaSyCaloVzVDnSmcwjtgy5fefBz7F60ubdL14';
+  const playlistId = 'PLXOKCNa-yVR03hHZbc5nw-HVbrp3bcg5y';
+
+  // Fetch latest videos from the playlist
+  fetch(`https://www.googleapis.com/youtube/v3/playlistItems?key=${apiKey}&playlistId=${playlistId}&part=snippet,contentDetails&order=date&maxResults=10`)
+    .then(response => response.json())
+    .then(data => {
+      const videosContainer = document.getElementById('videos-container');
+      
+      // Iterate through the playlist items and display the videos
+      data.items.forEach(item => {
+        const videoId = item.contentDetails.videoId;
+        const videoTitle = item.snippet.title;
+        const videoThumbnail = item.snippet.thumbnails.default.url;
+
+        const videoElement = document.createElement('div');
+        videoElement.innerHTML = `
+          <h2 class="videoTitle" title="${videoTitle}">${videoTitle}</h2>
+          <iframe class="vid" src="https://www.youtube.com/embed/${videoId}" frameborder="0" denyfullscreen></iframe>
+        `;
+        videoElement.setAttribute('id', 'videoDiv');
+
+        videosContainer.appendChild(videoElement);
+      });
+    })
+    .catch(error => console.error('Error fetching playlist videos:', error));
